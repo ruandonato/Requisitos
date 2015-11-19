c129306d46cabc3306b4423b1e036ff85d5e50de
Ruan Donato <ruandonato@outlook.com>
2015-11-19 16:56:53 -0200

    Adding association between project/story and standardization status of stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -70,6 +70,6 @@ class StoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def story_params
-      params.require(:story).permit(:role, :action, :reason, :description , :status, :estimate, :acceptance, :observations, :task, :release)
+      params.require(:story).permit(:role, :action, :reason, :description , :status, :estimate, :acceptance, :observations, :task, :release, :project_id)
     end
 end
