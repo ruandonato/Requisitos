99a7e1437726fe146c3cd5bbf0ad254ef4c0f1fc
Paulo Tada <paulohtfs@gmail.com>
2015-11-27 22:23:13 -0200

    Adding seeds to the project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+project_list = [
+  ["i-Educar", "Caroline", "i-Educar é um software público para a gestão escolar"],
+  ["SGD","Walker","Sistema de Gestão de Demanda"],
+  ["Mezuro","Manzo","Plataforma de medição e qualidade de software"],
+  ["Noosfero","Antonio", "Ferramenta para rede social livre"]
+]
+
+story_list = [
+  ["Role","Action", "Reason", "Status","Estimate","Acceptance", "Observations", "Task","Release", "Project"],
+  ["usuário","visualizar em quais programas um determinado órgão investiu","ver a relação destes programas com os órgãos públicos","To do","3 Pontos","Nome dos programas, órgão e investimento", "Utilizar dados públicos do orgão XPTO", "Realizar busca em banco, contruir campos na view"],
+  ["usuário","visualizar quais empreses foram contratadas por determinado órgão público", "ver a relação destas empresas com os órgãos públicos", "To do", "3 Pontos", "Nome da empresa, CNPJ, status de contratação", "Dado público do orgão XPTO","Realizar busca em banco, construir view"]
+]
+project_list.each do |name,owner,description|
+  Project.create(name: name, owner: owner, description: description)
+end
+
+story_list.each do |role,action,reason,status,estimate,acceptance,observations|
+  description = "Eu, como " + role + ", desejo " + action + " para " + reason
+  Story.create(role: role, action: action, reason: reason, description: description, status: status, estimate: estimate, acceptance: acceptance, observations: observations)
+end
